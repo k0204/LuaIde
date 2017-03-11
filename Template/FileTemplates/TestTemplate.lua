93707603fad4ca9fd96ac12e1df1d580adc71977
k0204 <1471768133@qq.com>
2017-03-11 21:49:22 +0800

    Create TestTemplate.lua

---
+++ Template/FileTemplates/TestTemplate.lua
@@ -0,0 +1,27 @@
+--
+--版权所有:{company}
+-- Author:{author}
+-- Date: {time}
+--
+local {moduleName} = class("{moduleName}")
+function  ${moduleName}:ctor()
+    self:loadCCS("UI/test/bag.json")
+    self:registerVarsByNames(self,vars)
+    self:registerBtns({
+        self.btnHero,
+        self.btnEquipment,
+        self.btnProps,
+    },function(sender) 
+        self:buttonClick(sender)    
+    end)
+end
+--desc:
+--Author:{author}
+--date:{time}
+function  {moduleName}:buttonClick(sender)
+    
+end
+function  {moduleName}:test1()
+   
+end
+return  {moduleName}
